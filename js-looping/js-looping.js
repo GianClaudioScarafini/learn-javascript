ddd56daae006975a3863a670d3d21085f51267d3
GianClaudioS <g.claudio.scarafini@gmail.com>
2024-04-09 12:53:22 +0100

    looping

---
+++ js-looping/js-looping.js
@@ -0,0 +1,15 @@
+// lopping what is that? 
+// looping is a way to repeat a block of code multiple times.
+
+
+const thisIsMyArray = ["Milan", "Raj", "Rajesh", "Rajendra"];
+
+console.log(thisIsMyArray[0]);
+console.log(thisIsMyArray[1]);
+console.log(thisIsMyArray[2]);
+console.log(thisIsMyArray[3]);
+
+console.log("thisIsMyArray");
+console.log("--------------------------------- \n");
+
+// this is the wrong way to print the array.
